b014e330b6a7b058795e6d975f2fef298793a62a
Daniel Santiago Pérez <danielsantiagoperez03@gmail.com>
2024-10-08 18:46:48 -0500

    Dashboard page finished

---
+++ src/pages/Dashboard.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page class="flex flex-center q-px-md">
+    <img src="~assets/hermes-software-logo-slogan.svg" width="400px" height="auto" >
+  </q-page>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+</style>
