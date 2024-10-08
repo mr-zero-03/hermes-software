38f18307142eb28ec6010d9ef57f5d1ccaad9364
Daniel Santiago Pérez <danielsantiagoperez03@gmail.com>
2024-10-08 09:06:36 -0500

    Contact page finished
    - Contact form component added

---
+++ src/components/contact/Form.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="full-width">
+    <q-card
+      tag="form"
+      @submit="onSubmit"
+      class="q-pa-sm q-py-md shadow-6 contact-form"
+    >
+      <q-card-section>
+        <div class="form-input q-pb-sm">
+          <span>Nombre</span>
+          <q-input class="q-pt-sm" standout outlined v-model="dataForm.name" dense />
+        </div>
+
+        <div class="form-input q-pb-sm">
+          <span>Email</span>
+          <q-input class="q-pt-sm" standout outlined v-model="dataForm.email" dense />
+        </div>
+
+        <div class="form-input q-pb-sm">
+          <span>Mensaje</span>
+          <q-input type="textarea" class="q-py-sm" standout outlined v-model="dataForm.message" :input-style="{height: '80px'}" />
+        </div>
+      </q-card-section>
+
+      <q-card-actions>
+        <q-btn color="primary" no-caps class="full-width">Enviar</q-btn>
+      </q-card-actions>
+    </q-card>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      dataFormBase: {
+        name: '',
+        email: '',
+        message: '',
+        date: null
+      },
+      dataForm: { ...this.dataFormBase }
+    }
+  },
+  methods: {
+    onSubmit () {
+      if (accept.value !== true) {
+        $q.notify({
+          color: 'red-5',
+          textColor: 'white',
+          icon: 'warning',
+          message: 'You need to accept the license and terms first'
+        })
+      }
+      else {
+        $q.notify({
+          color: 'green-4',
+          textColor: 'white',
+          icon: 'cloud_done',
+          message: 'Submitted'
+        })
+      }
+    },
+    onReset () {
+      this.dataForm = { ...this.dataFormBase }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .contact-form {
+    border: 1px solid $primary;
+  }
+</style>
---
+++ src/pages/contact/Contact.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex flex-center q-px-lg">
+    <h2 class="text-h3 full-width text-center q-py-lg">Ponte en contacto con nosotros</h2>
+
+    <ContactForm style="max-width: 570px; padding-bottom: 120px;" />
+  </q-page>
+</template>
+
+<script>
+import ContactForm from 'components/contact/Form.vue'
+
+export default {
+  components: {
+    ContactForm
+  }
+}
+</script>
